bdcdd0324ea15913171f579e5d03dc3537c4d167
Guenther Hoene <guntherhone@outlook.com>
2018-11-13 09:27:30 +0100

    websocket integrated

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
 
 const app = express();
 
 app.use(express.static("static"));
 
-app.listen(5555, () => {
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+let heading = 0;
+
+setInterval(() => {
+  heading = (heading + 1) % 360;
+  let serializedHeading = JSON.stringify({ heading });
+  wss.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(serializedHeading);
+    }
+  });
+}, 15);
+
+wss.on("connection", (ws, req) => {
+  ws.on("close", (socket, code, reason) =>
+    console.log(`Connection to ${req.connection.remoteAddress} closed.`)
+  );
+  console.log(`${req.connection.remoteAddress}`);
+});
+
+server.listen(5555, () => {
   console.log("listening on port 5555");
 });
--- static/script.js
+++ static/script.js
@@ -1,61 +1,53 @@
-let needle;
-let arcLeft;
-let arcRight;
-let compassDisc;
-let compassHeadings;
-let heading;
+(() => {
+  const needle = document.querySelector(".needle");
+  const arcLeft = document.querySelector(".arc.left");
+  const arcRight = document.querySelector(".arc.right");
+  const compassDisc = document.querySelector("#compass-disc");
+  const compassHeadings = document.querySelectorAll("#compass-headings text");
+  const heading = document.querySelector("#heading");
 
-/***********/
-function setRotationValue(event) {
-  window.requestAnimationFrame(() => {
-    needle.setAttribute("transform", `rotate(${event.target.value})`);
+  /***********/
+  function setRotationValue(event) {
+    window.requestAnimationFrame(() => {
+      needle.setAttribute("transform", `rotate(${event.target.value})`);
+    });
+  }
+
+  /***********/
+  function setDangerAngle(event) {
+    const deg = +event.target.value;
+    const x = -180.0 * Math.sin((deg * Math.PI) / 180.0);
+    const y = -180.0 * Math.cos((deg * Math.PI) / 180.0);
+
+    arcLeft.setAttribute("d", `M${x} ${y} A180 180 0 0 1 -61.56 -169.14`);
+
+    arcRight.setAttribute("d", `M61.56 -169.14 A180 180 0 0 1 ${-x} ${y}`);
+  }
+
+  /***********/
+  addEventListener("deviceorientation", function(event) {
+    const headingValue = event.webkitCompassHeading || 0;
+
+    window.requestAnimationFrame(() => {
+      compassDisc.setAttribute("transform", `rotate(-${headingValue})`);
+
+      compassHeadings.forEach((heading, i) =>
+        heading.setAttribute(
+          "transform",
+          `rotate(${i * 30}) rotate(${-(i * 30 - headingValue)},0,140)`
+        )
+      );
+
+      heading.innerHTML = `${headingValue | 0}&deg;`;
+    });
   });
-}
 
-/***********/
-function setDangerAngle(event) {
-  const deg = +event.target.value;
-  const x = -180.0 * Math.sin((deg * Math.PI) / 180.0);
-  const y = -180.0 * Math.cos((deg * Math.PI) / 180.0);
+  const ws = new WebSocket(`ws://${location.host}`);
+  ws.onmessage = event => {
+    let data = JSON.parse(event.data);
+    window.requestAnimationFrame(() => {
+      needle.setAttribute("transform", `rotate(${data.heading})`);
+    });
+  }
 
-  arcLeft.setAttribute("d", `M${x} ${y} A180 180 0 0 1 -61.56 -169.14`);
-
-  arcRight.setAttribute("d", `M61.56 -169.14 A180 180 0 0 1 ${-x} ${y}`);
-}
-
-/***********/
-addEventListener("load", function(e) {
-  needle = document.querySelector(".needle");
-
-  arcLeft = document.querySelector(".arc.left");
-
-  arcRight = document.querySelector(".arc.right");
-
-  compassDisc = document.querySelector("#compass-disc");
-
-  compassHeadings = document.querySelectorAll("#compass-headings text");
-
-  heading = document.querySelector("#heading");
-
-  console.log(compassHeadings.length);
-
-  /*setRotationValue({target:document.querySelector(".direction-input")})
-
-setDangerAngle({target:document.querySelector(".danger-input")})*/
-});
-
-/***********/
-addEventListener("deviceorientation", function(event) {
-  const headingValue = event.webkitCompassHeading;
-
-  compassDisc.setAttribute("transform", `rotate(-${headingValue})`);
-
-  compassHeadings.forEach((heading, i) =>
-    heading.setAttribute(
-      "transform",
-      `rotate(${i * 30}) rotate(${-(i * 30 - headingValue)},0,140)`
-    )
-  );
-
-  heading.innerHTML = `${headingValue | 0}&deg;`;
-});
+})();
